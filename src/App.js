00e12741a5c4fdc315b219d4e3ed47bcd93003b3
muhamaad idrees <midreeskhan7276@gmail.com>
2022-02-07 13:16:45 -0800

    working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import react, { useState } from "react"
+import "./index.css"
+const App = _ => {
+  const [myStyle, setStyle] = useState({
+    color: 'black',
+    backgroundColor: "white",
+    // width : "100%",
+    // height : "500px"
+  })
+  const [btn, setBtn] = useState("Dark Mood")
+  const change = _ => {
+    if (myStyle.color == "black") {
+      setStyle({
+        color: 'white',
+        backgroundColor: "black"
+      })
+      setBtn("light Mood")
+    }
+    else {
+            
+      setStyle({
+        color: 'black',
+        backgroundColor: "white"
+      })
+      setBtn("dark Mood")
+    }
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={myStyle} className="main">
+
+        <button onClick={change} >{btn}</button>
+      </div>
+    </>
   );
 }
 
